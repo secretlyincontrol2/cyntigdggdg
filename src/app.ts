de98c4aa764d3b2eebd96be643ecab42ff2ad7e5
BU22-2130 <bu22-2130@babcock.edu>
2026-03-23 21:38:42 +0100

    Use 127.0.0.1 for proxy v1.0.7

--- src/app.ts
+++ src/app.ts
@@ -80,7 +80,7 @@ app.use('/api/flashcards', flashcardRoutes);
 
 // Test route
 app.get('/', (req, res) => {
-    res.send('BUPT-AI API is running v1.0.6-DATABASE-FIX ✅ (Proxy Active)');
+    res.send('BUPT-AI API is running v1.0.7-IPV4-FIX ✅ (Proxy Active)');
 });
 
 export default app;
